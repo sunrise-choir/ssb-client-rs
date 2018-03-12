88602eea67fb3e7eb05b60a3bc7f48c16f90e2b8
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-03-12 17:44:32 +0100

    Fix get to correctly return messages

--- src/ssb.rs
+++ src/ssb.rs
@@ -6,6 +6,7 @@ use muxrpc::{InSyncResponse, InAsyncResponse, ConnectionRpcError};
 use serde::de::DeserializeOwned;
 use serde_json::Value;
 use ssb_common::links::MessageId;
+use ssb_common::messages::Message;
 use ssb_rpc::ssb::{Whoami as RpcWhoami, WhoamiResponse, Get as RpcGet};
 
 use super::{Client, SendRpc};
@@ -36,15 +37,17 @@ impl<R: AsyncRead> Future for Whoami<R> {
 pub fn get<R: AsyncRead, W: AsyncWrite, T: DeserializeOwned>(client: &mut Client<R, W>,
                                                              id: MessageId)
                                                              -> (SendRpc<W>, Get<R, T>) {
-    let (req, res) = client.0.async::<RpcGet, T, Value>(&RpcGet::new(id));
+    let (req, res) = client
+        .0
+        .async::<RpcGet, Message<T>, Value>(&RpcGet::new(id));
     (SendRpc::new_async(req), Get(res))
 }
 
 /// Get a `Message` by its `MessageId`.
-pub struct Get<R: AsyncRead, T: DeserializeOwned>(InAsyncResponse<R, T, Value>);
+pub struct Get<R: AsyncRead, T: DeserializeOwned>(InAsyncResponse<R, Message<T>, Value>);
 
 impl<R: AsyncRead, T: DeserializeOwned> Future for Get<R, T> {
-    type Item = T;
+    type Item = Message<T>;
     type Error = ConnectionRpcError<Value>;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
